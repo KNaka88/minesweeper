14f78c34ec41ed63b387cd67e00a37777122edb8
Jake Campa and Koji Nakagawa <jake+koji@>
2017-04-05 15:11:23 -0700

    finish minesweeper

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -9,18 +9,19 @@ import { Mine } from '../mine.model';
 export class BoardComponent implements OnInit {
   mineBoard: any[] = [];
   gridDimensions: number = 10;
+  showFlag: true;
 
 
   constructor() { }
   setMine() {
-    let number = this.gridDimensions
+    let number = this.gridDimensions;
     for (let r = 0; r < number; r++) {
       this.mineBoard[r] = [];
 
       for (let c = 0; c < number; c++) {
         let rng = Math.random();
         let isMine = false;
-        if (rng < .1) {
+        if (rng < .17) {
           isMine = true;
         }
         let newMine = new Mine(r, c, isMine);
@@ -31,56 +32,78 @@ export class BoardComponent implements OnInit {
   }
 
   ngOnInit() {
-    // var number: number = 8;
-    // for(let r=0; r<number; r++){
-    //   for(let c=0; c<number; c++){
-    //     var newMine =new Mine(r, c, 9);
-    //     console.log(newMine);
-    //   }
-    // }
-    // this.mineBoard.push(newMine);
     this.setMine();
   }
 
+  colorText(col) {
+    if (col.minesAdjacent === 1) {
+      return "text-success border";
+    } else if (col.minesAdjacent === 2) {
+      return "text-primary border";
+    } else if (col.minesAdjacent === 3) {
+      return "text-danger border";
+    } else {
+      return "border";
+    }
+  }
+ checkWin() {
+   for (let i = 0; i < this.mineBoard.length; i++){
+     for (let j = 0; j < this.mineBoard[i].length; j++) {
+         let currentSquare = this.mineBoard[i][j];
+         console.log(i + " " + j);
+         if (currentSquare.mine === false && currentSquare.revealed === false) {
+           return;
+         }
+
+     }
+   }
+    alert('win');
+ }
   checkMine(row, col) {
-
+    let currentSquare = this.mineBoard[row][col];
     //Check if it is mine or not
-    this.mineBoard[row][col].isMine();
-
-
-
+    currentSquare.isMine();
+    currentSquare.reveal(true);
+    this.checkWin();
     //Check neighbors board
-
-
+    let myNeighbors = [];
     let numberToDisplay = 0;
     for (let i = -1; i <= 1; i++) {
       for (let j = -1; j <= 1; j++) {
         if (row + i !== -1 && col + j !== -1 && row + i !== this.gridDimensions && col + j !== this.gridDimensions) {
           let neighbor = this.mineBoard[row + i][col + j];
-          console.log(neighbor);
+          myNeighbors.push(neighbor);
           if (neighbor.mine) {
             numberToDisplay++;
           }
         }
       }
-
-      this.mineBoard[row][col].setMinesAdjacent(numberToDisplay);
-      console.log(numberToDisplay);
-      if (numberToDisplay === 0) {
-        for (let i = -1; i <= 1; i++) {
-          for (let j = -1; j <= 1; j++) {
-            if (row + i !== -1 && col + j !== -1 && row + i !== this.gridDimensions && col + j !== this.gridDimensions) {
-              this.checkMine(row + i, col + j);
-            }
-          }
-        }
-      }
-
     }
-    // setMine(row, col){
-    //   let newMine =new Mine(row, col, 9);
-    //   this.mineBoard.push(newMine);
-    // }
+    currentSquare.setMinesAdjacent(numberToDisplay);
+     if(currentSquare.minesAdjacent === 0) {
+       for (let k = 0; k < myNeighbors.length; k++) {
+         if(!myNeighbors[k].revealed)
+           this.checkMine(myNeighbors[k].row, myNeighbors[k].col);
+       }
+     }
+  }
+
+
+  flag(r, c, event) {
+    event.preventDefault();
+
+    let currentSquare = this.mineBoard[r][c];
+    if (currentSquare.minesAdjacent === "⚐"){
+      currentSquare.minesAdjacent = ""
+      currentSquare.reveal(false);
+    } else if (currentSquare.revealed){
+      // do nothing
+    } else {
+      currentSquare.minesAdjacent = "⚐"
+      currentSquare.reveal(true);
+    }
+
 
   }
+
 }
--- src/app/mine.model.ts
+++ src/app/mine.model.ts
@@ -1,11 +1,14 @@
 export class Mine {
-  public minesAdjacent: any = null;
-  constructor(public row: number, public col: number, public mine: boolean, public  revealed: boolean = false) { }
+  public minesAdjacent: any;
+  constructor(public row: number, public col: number, public mine: boolean, public revealed: boolean = false) { }
 
   isMine() {
     if (this.mine) {
       alert("You are a LOSER!");
     }
+  }
+  reveal(bool: boolean) {
+    this.revealed = bool;
   }
 
   setMinesAdjacent(num: number) {
